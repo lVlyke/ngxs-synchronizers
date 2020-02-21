a995ecad79571bfcb34bc390c5c1a8ede6b1a2ac
Mychal Thompson <mychal.r.thompson@gmail.com>
2020-02-20 22:14:50 -0500

    Corrected tslint issue

--- spec/test.spec.ts
+++ spec/test.spec.ts
@@ -1,6 +1,5 @@
 import { PropertySynchronizer } from "./../src/synchronizer/property-synchronizer";
 
-
 describe("Test", () => {
     it("works", () => expect(PropertySynchronizer).toBeTruthy());
 });
